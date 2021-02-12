e19dc84226f126d08fd3bb553e6de54f5074d9db
sachitsaksena <sachit@mit.edu>
2021-02-12 18:19:09 -0500

    minor update

--- prescient/cmd/simulate.py
+++ prescient/cmd/simulate.py
@@ -1,23 +1,19 @@
 import argparse
+import torch
 import prescient.simulate as simulate
 from prescient.train.model import *
-
-
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--data_path", required=True)
-
     parser.add_argument("--model_path", required=True)
     parser.add_argument("--seed", default=1, required=True)
-
     parser.add_argument("--num_sims", default=10)
     parser.add_argument("--num_steps", default=None, required=False)
+    parser.add_argument("-o", "--path_to_output", default=None)
 
     # load data
     data_pt = torch.load(args.data_path)
-    
-
 
     # load model
     config_path = os.path.join(args.model_path, '/seed_{}/config.pt'.format(args.seed))
@@ -34,7 +30,12 @@ def main():
         num_steps = int(np.round(data_pt["y"] / config.train_dt))
 
     # simulate forward
-    simulate(data_pt, model, config, num_sims, num_cells, num_steps, tp=None, celltype=None, gpu=None)
+    sims = simulate(data_pt, model, config, num_sims, num_cells, num_steps, tp=None, celltype=None, gpu=None)
+
+    # write simulation data to file
+    torch.save({
+    "sims": sims
+    }, args.path_to_output)
 
 if __name__=="__main__":
     main()
