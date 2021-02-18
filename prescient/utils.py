2b762abc7444eed5356b0f7db07ebd7c0ae377ac
sachitsaksena <sachit@mit.edu>
2021-02-18 00:29:30 -0500

    update growth utils and notebooks

--- prescient/utils.py
+++ prescient/utils.py
@@ -63,39 +63,75 @@ def compute_growth(L0, L, k, birth_score, death_score, birth_smoothed_score, dea
     g = b - d
     return g
 
-def get_growth_weights(x, xp, y, genes, birth_gst, death_gst, **kwargs):
+def get_growth_weights(x, xp, metadata, tp_col, genes, birth_gst, death_gst, outfile,
+                       n_neighbors=20, beta=0.1, L0=0.3, L=1.1, k=0.001):
     """
     Estimate growth using KEGG gene annotations. Implements smoothing procedure.
 
     Inputs:
     -------
-    x: numpy ndarray of gene expression.
+    x: numpy ndarray of scaled gene expression.
+    xp: numpy ndarray of PCs.
     genes: list or numpy array of highly variable gene symbols.
-    birth_gst: birth signature annotations.
+    birth_gst: path to csv of birth signature annotations.
+    death_gst: path to csv of death signature annotations.
+    outfile: provide name of outfile pt.
 
     Outputs:
     --------
     weights: growth rates vector.
     """
-
-    birth_gst = [g for g in birth_gst['gene_symbol'].unique() if g in genes]
-    gst = pd.read_csv(gst, index_col = 0)
-    death_gst = [g for g in death_gst['gene_symbol'].unique() if g in genes]
+    gst = pd.read_csv(birth_gst, index_col=0)
+    birth_gst = [g for g in gst['gene_symbol'].unique() if g in genes]
+    gst = pd.read_csv(death_gst, index_col = 0)
+    death_gst = [g for g in gst['gene_symbol'].unique() if g in genes]
 
     birth_gst = [g for g in birth_gst if g not in death_gst]
     death_gst = [g for g in death_gst if g not in birth_gst]
 
-    # smoothing procedure for converting to pcs
-    ay = annoy.AnnoyIndex(xp_.shape[1], 'euclidean')
-    for i in range(xp_.shape[0]):
-        ay.add_item(i, xp_[i])
+    # smoothing procedure for growth
+    ay = annoy.AnnoyIndex(xp.shape[1], 'euclidean')
+    for i in range(xp.shape[0]):
+        ay.add_item(i, xp[i])
     ay.build(10)
 
+    prev_score = x[birth_gst].mean(axis = 1).values
+    cur_score = np.zeros(prev_score.shape)
+
+    for _ in range(5):
+        for i in range(len(prev_score)):
+            xn = prev_score[ay.get_nns_by_item(i, 20)]
+            cur_score[i] = (beta * xn[0]) + ((1 - beta) * xn[1:].mean(axis = 0))
+        prev_score = cur_score
+
+    birth_score = x[birth_gst].mean(axis = 1).values
+    birth_smoothed_score = cur_score
+
+    # smooth death score
+
+    prev_score = x[death_gst].mean(axis = 1).values
+    cur_score = np.zeros(prev_score.shape)
+
+    for _ in range(5):
+        for i in range(len(prev_score)):
+            xn = prev_score[ay.get_nns_by_item(i, 20)]
+            cur_score[i] = (beta * xn[0]) + ((1 - beta) * xn[1:].mean(axis = 0))
+        prev_score = cur_score
+
+    death_score = x[death_gst].mean(axis = 1).values
+    death_smoothed_score = cur_score
+
     # compute growth
-    g = compute_growth(L0, L, k)
-    if args.growth_parameters != None:
-        growth_args = str(args.growth_parameters).split(',')
-        w = estimate_growth(x, xp, genes, args.growth_annotation, L0=growth_args[0], L=growth_args[1], k=growth_args[2])
-    else:
-        w = estimate_growth(x, xp, genes, y, args.growth_annotation)
-    return weights
+    g = compute_growth(L0, L, k,
+                       birth_score, death_score,
+                       birth_smoothed_score, death_smoothed_score)
+    y_l = sorted(metadata[tp_col].unique())
+    g_l = [g[(metadata[tp_col] == y_).values] for y_ in y_l]
+
+    # write growth
+    torch.save({
+        "w": g_l
+    }, outfile)
+
+    return g, g_l
+    
