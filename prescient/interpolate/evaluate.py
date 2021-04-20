1c4c2f61713c660f3b540afbc0f03aab8a0eedc7
Sachit Saksena <sachit@mit.edu>
2021-04-20 14:34:16 -0400

    debug evaluate_interpolate data

--- prescient/interpolate/evaluate.py
+++ prescient/interpolate/evaluate.py
@@ -14,41 +14,46 @@ def w2dist(x, y):
     --------
     loss_xy: W-2 distance between observed and simulated points at the heldout timepoint.
     """
-    ot_solver = SamplesLoss("sinkhorn", p = 2, blur = config.sinkhorn_blur,
-        scaling = config.sinkhorn_scaling)
-    distance = ot_solver(x_i, y_j)
-    return distance
 
-def evaluate_interpolate_data(y_j, data_pt_path, simulate_pt_path, num_steps):
+
+def evaluate_interpolate_data(y, data_pt_path, simulate_pt_path, num_steps, gpu=None):
     """
     Approximates W-2 distance between sets of points using Sinkhorn as implemented in GeomLoss.
 
     Arguments
     ---------
-    - y_j: held-out timepoint (as Numpy ndarray normalized expression).
-    - data_pt_path: path to serialized torch object produced by "process_data" cmd.
-    - simulate_pt_path: path to serialized torch object produced by "simulate_trajectiories" cmd.
-
+    - y -> numpy.ndarray: held-out timepoint (as Numpy ndarray normalized expression).
+    - data_pt_path -> str: path to serialized torch object produced by "process_data" cmd.
+    - simulate_pt_path -> str: path to serialized torch object produced by "simulate_trajectiories" cmd.
+    - num_steps -> int: number of steps taken with "simulate_trajectories". (i.e the number of steps to get to the held-out timepoint)
+    - gpu -> int: if CUDA-enabled gpu enabled provide GPU number (i.e if cuda:0 is availale, provide 0) -> int
     Returns:
     --------
-    loss_xy: W-2 distance between observed and simulated points at the heldout timepoint.
+    interpolation_distances -> list(torch.Tensors): W-2 distance between observed and simulated points at the heldout timepoint.
     """
 
+    if gpu != None:
+        device = torch.device('cuda:{}'.format(gpu))
+    else:
+        device = torch.device('cpu')
+
     # load PRESCIENT outputs
-    data_pt = torch.load(dat_pt_path)
+    data_pt = torch.load(data_pt_path)
     simulate_pt = torch.load(simulate_pt_path)
 
     # transform held-out timepoint using PCA from data_pt
     pca = data_pt["pca"]
-    y_j_ = pca.transform(y_j)
-    y_j = x[1].to(device)
+    y_ = torch.from_numpy(pca.transform(y)).float().to(device)
 
     # evaluate interpolation distance for each simulation in simulate_pt
     sims = simulate_pt["sims"]
     interpolate_distances=[]
+    ot_solver = SamplesLoss("sinkhorn", p = 2, blur = config.sinkhorn_blur,
+        scaling = config.sinkhorn_scaling)
+    return distance
     for sim in sims:
-        x_i = torch.from_numpy(sim[:, :, :, num_steps]).float().to(device)
-        interpolate_distances.append(w2dist(x_i, y_j))
+        x_i = torch.from_numpy(sim[num_steps,:,:]).float().to(device)
+        interpolate_distances.append(ot_solver(x_i, y_))
     return interpolate_distances
 
 
